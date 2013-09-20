1ac328b01fba1e32ec0f15a379388ae7446e2274
Todd Carter <todd.carter@springer.com>
2013-09-20 17:25:53 +0100

    TC: First version without typography styles

--- assets/js/parallax.js
+++ assets/js/parallax.js
@@ -1,4 +1,3 @@
-
 (function(){
 
 	var cacheObj={},
@@ -16,12 +15,10 @@
 
 		//caching non-changing values...
 		cacheObj={
-			$body:$("body"),
 			parFx:parFx,
 			panels:panels,
 			$w:$w,
-			panelHeight:panelHeight,
-			images:panels.find("img")
+			panelHeight:panelHeight
 		}
 			
 		// setting the events handler
@@ -43,6 +40,7 @@
 		})
 
 		//running the functions on page ready
+		setResponsiveImages();
 		setImageRatio();
 		setActivePanel();
 		setBGposition();
@@ -69,16 +67,19 @@
 		cacheObj.panels.each(function(index){
 			var $this=$(this),
 				thisPos=$this.position(),
-				imgObj=cacheObj.images.eq(index),
+				imgObj=$this.find("img"),
+				isNotVisible=(thisPos.top>(windowHeight+WscrollTop)||(thisPos.top+$this.innerHeight())<WscrollTop);
 
+			if (!isNotVisible){
 				// height diff between the panel and the image
-				imageDeltaH=imgObj.innerHeight()-cacheObj.panelHeight;
-
+				var imageDeltaH=imgObj.innerHeight()-cacheObj.panelHeight;
 
 				var top=imageDeltaH/(windowHeight/(WscrollTop-thisPos.top));
 				imgObj.css({
 					top:top
 				})
+				
+			}
 		})
 	}
 
@@ -98,5 +99,52 @@
 		})
 	}
 	
+    function setResponsiveImages() {
+    		//looking for the noscript tag
+        var responsiveImages = $(".responsive"),
+        	//checking the retina display
+			isRetina = (
+				window.devicePixelRatio > 1 ||
+				(window.matchMedia && window.matchMedia("(-webkit-min-device-pixel-ratio: 1.5),(-moz-min-device-pixel-ratio: 1.5),(min-device-pixel-ratio: 1.5)").matches)
+			),  	
+			//setting the break-points
+        	mqBreakPoints = {
+        		"_smartphone":600,
+        		"_tablet":1024, 
+        		"_widescreen":99999
+        	},
+        	imgFormat="";
+
+
+        //test for available width in current browser window
+        for (var bp in mqBreakPoints){
+	        if(cacheObj.$w.innerWidth() < mqBreakPoints[bp]){ 
+	          imgFormat = bp;
+	          break;
+	        }
+        }
+
+        imgFormat=isRetina?imgFormat+"@2x":imgFormat;
+        
+        //creating the IMG element and adding it to the DOM
+        responsiveImages.each(function(){
+
+			var noScriptElem=$(this),
+				imgUrl=noScriptElem.data("baseimg"),
+				img = window.document.createElement("img"),
+				$img=$(img).addClass('board__image');
+
+			//setting the image as soon as it gets loaded
+			$img.on("load",function(){
+				setBGposition();
+			});
+
+			img.src=imgUrl + imgFormat + ".jpg";
+			$img.insertAfter(noScriptElem);
+
+        })
+
+    }
+
 })();
 
