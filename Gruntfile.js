f70f71d466c3c5be26785dfa0917f1f5739040a1
robL <rob.lloyd@springer.com>
2013-09-26 14:04:51 +0100

    RobL:Added minification of js and css

---
+++ Gruntfile.js
@@ -0,0 +1,48 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    clean: ["assets/css/min", "assets/js/min"],
+    uglify: {
+      options: {
+        banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+
+      },
+      dist: {
+        files:[
+          {src: ['assets/js/*'],dest:'assets/js/min/joinit-springer.min.js'}
+        ]
+      }
+    },
+
+    cssmin: {
+      minify: {
+        files: {
+          'assets/css/min/base.min.css': ['assets/css/base.css'],
+          'assets/css/min/joinit.springer.min.css': ['assets/css/joinit.springer.css']
+        }
+      }
+    }
+
+  });
+
+
+
+  // Load the plugin that provides the "uglify" task.
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+
+  // Load the plugin that provides the "cssmin" task.
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+
+  // Load the plugin that provides the "clean" task.
+  grunt.loadNpmTasks('grunt-contrib-clean');
+
+  grunt.loadNpmTasks('grunt-contrib-compress');
+
+
+  // Default task(s).
+  grunt.registerTask('default', ['clean','uglify','cssmin','compress']);
+
+
+};
